7983b51cc469592b855d89a1ed3df62d630b82f7
Austin Gilmore <austingilmora@gmail.com>
2021-10-26 15:34:36 -0400

    finished adding tasks as a form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
-var formE1 = document.querySelector("#task-form");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //make div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
+
 };
 
-formE1.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
